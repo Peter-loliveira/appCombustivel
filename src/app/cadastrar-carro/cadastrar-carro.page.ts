4e0a92e75668df9e70b8126807b697517ab7437a
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-11-16 11:51:37 -0300

    Opção de aviso de Aguarde
    Inserido componente de aviso visual que a requisição de gravar o grarro no DB está sendo feita

--- src/app/cadastrar-carro/cadastrar-carro.page.ts
+++ src/app/cadastrar-carro/cadastrar-carro.page.ts
@@ -1,30 +1,41 @@
-import { CarroService } from 'src/services/CarroService';
-import { Carro } from 'src/models/Carro';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { CarroService } from "src/services/CarroService";
+import { Carro } from "src/models/Carro";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-cadastrar-carro',
-  templateUrl: './cadastrar-carro.page.html',
-  styleUrls: ['./cadastrar-carro.page.scss'],
+  selector: "app-cadastrar-carro",
+  templateUrl: "./cadastrar-carro.page.html",
+  styleUrls: ["./cadastrar-carro.page.scss"],
 })
 export class CadastrarCarroPage implements OnInit {
+  public carro: Carro = new Carro();
+  private _htmlLoading: HTMLIonLoadingElement
 
-  public carro: Carro = new Carro()
-  constructor( 
+  constructor(
     public _carrosService: CarroService,
     private _router: Router,
-     ) {
-    
-   }
+    private _loading: LoadingController
+  ) {}
 
   ngOnInit() {}
 
-  cadastrarCarro(){
-    this._carrosService.cadastrar(this.carro).subscribe(res => {
-      console.log(res);
-      this._router.navigate(['/lista-carros'])
-    })  
+  async carregando() {
+    // Criar Loading para sugerir que ainda está tentando criar o carro no DB
+    this._htmlLoading = await this._loading.create({
+      cssClass: "my-custom-class",
+      message: "Aguarde...",
+    });
+    await this._htmlLoading.present();
   }
 
+  cadastrarCarro() {
+    this.carregando();
+    this._carrosService.cadastrar(this.carro).subscribe((res) => {
+      console.log(res);
+      this._htmlLoading.dismiss(); 
+      this._router.navigate(["/lista-carros"]);
+    });
+  }
 }
