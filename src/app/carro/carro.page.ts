b3ded86cd6d436ef72fef06f4bf9493882f57b2a
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-11-18 18:20:48 -0300

    Detalhes dos Carros
    Exibição dos detelhes do Carro selecionado na pag Lista de Carros

--- src/app/carro/carro.page.ts
+++ src/app/carro/carro.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { CarroService } from "src/services/CarroService";
+import { Carro } from "src/models/Carro";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-carro',
-  templateUrl: './carro.page.html',
-  styleUrls: ['./carro.page.scss'],
+  selector: "app-carro",
+  templateUrl: "./carro.page.html",
+  styleUrls: ["./carro.page.scss"],
 })
 export class CarroPage implements OnInit {
+  public carro_id: Number;
+  public carro: Carro = new Carro();
+  public ListaCarros: Carro[] = new Array<Carro>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private _carroService: CarroService,
+    private _router: ActivatedRoute,
+    private _nav: Router
+  ) {
+    this.ListaCarros = this._carroService.carros;
+    this._router.params.subscribe((params) => {
+      this.carro_id = parseInt(params.carroId);
+      console.log(this.carro_id);
+      this.obterCarro();
+    });
   }
 
+  ngOnInit() {}
+
+  obterCarro() {
+    const carroEncntrado = this.ListaCarros.find((c) => c.id === this.carro_id);
+    if (carroEncntrado) {
+      this.carro = carroEncntrado;
+    } else {
+      this._nav.navigate(["/lista-carros"]);
+    }
+  }
 }
